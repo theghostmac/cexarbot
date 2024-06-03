e86b46bb43609987a65ecbc27388132e12c87e3e
theghostmac <theghostmac@gmail.com>
2024-06-03 23:06:07 +0100

    [feature] completed the CLI setup for the bot

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,48 @@ fn cli() -> Command {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
-            Command::new("")
+            Command::new("trade")
+                .about("Executes a trade using a trading strategy.")
+                .arg(
+                    Arg::new("strategy")
+                        .short('s')
+                        .long("strategy")
+                        .value_name("STRATEGY")
+                        .help("Trading strategy to use.")
+                        .required(true)
+                )
+                .arg(
+                    Arg::new("symbol")
+                        .short('t')
+                        .long("symbol")
+                        .value_name("SYMBOL")
+                        .help("Trading pair symbol (e.g., BTCUSDT).")
+                        .required(true)
+                )
+        )
+        .subcommand(
+            Command::new("backtest")
+                .about("Back-testing a trading strategy.")
+                .arg(
+                    Arg::new("strategy")
+                        .short('s')
+                        .long("strategy")
+                        .value_name("STRATEGY")
+                        .help("The trading strategy to backtest.")
+                        .required(true)
+                )
+                .arg(
+                    Arg::new("symbol")
+                        .short('t')
+                        .long("symbol")
+                        .value_name("SYMBOL")
+                        .help("Trading pair symbol (e.g., BTCUSDT).")
+                        .required(true)
+                )
         )
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!(
         r#"
 
@@ -29,5 +66,24 @@ fn main() {
 
         "#
     );
-}
 
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("trade", sub_matches)) => {
+            let strategy = sub_matches.get_one::<String>("strategy").unwrap();
+            let symbol = sub_matches.get_one::<String>("symbol").unwrap();
+            println!("Executing {} strategy on {}", strategy, symbol);
+            // TODO: call the function to execute the trading strategy.
+        },
+        Some(("backtest", sub_matches)) => {
+            let strategy = sub_matches.get_one::<String>("strategy").unwrap();
+            let symbol = sub_matches.get_one::<String>("symbol").unwrap();
+            println!("Back-testing {} strategy on {}", strategy, symbol);
+            // TODO: call the function to execute a back-test on the trading strategy.
+        }
+        _ => unreachable!("The CLI should require a subcommand, this should never happen"),
+    }
+
+    Ok(())
+}
